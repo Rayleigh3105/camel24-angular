623ec9e516ebc4d521f6e76265b4993113e92e1b
mvogt <moritz.vogt@vogges.de>
2019-01-11 17:10:54 +0100

    - added shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { ContentLayoutsComponent } from './layouts/content-layouts/content-layouts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentLayoutsComponent,
   ],
   imports: [
     BrowserModule,
