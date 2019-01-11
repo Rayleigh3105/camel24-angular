2579732dd946a799223c3ae73f0be3d122a8c21b
mvogt <moritz.vogt@vogges.de>
2019-01-11 17:12:05 +0100

    - completed file structure

---
+++ src/app/layouts/content-layouts/content-layouts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'camel-content-layouts',
+  templateUrl: './content-layouts.component.html',
+  styleUrls: ['./content-layouts.component.scss']
+})
+export class ContentLayoutsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
