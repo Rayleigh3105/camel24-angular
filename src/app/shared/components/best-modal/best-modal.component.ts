cf520112ba227f3f1549715977542e957d8fbc38
mvogt <moritz.vogt@vogges.de>
2019-01-16 23:58:58 +0100

    - added new component modal

---
+++ src/app/shared/components/best-modal/best-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'camel-best-modal',
+  templateUrl: './best-modal.component.html',
+  styleUrls: ['./best-modal.component.scss']
+})
+export class BestModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
