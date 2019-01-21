c3fab96f396bc9fc2b51ed01950bd36ce0a177c1
Moritz Vogt <m.vogt@prosis.de>
2019-01-21 14:59:39 +0100

    - new color header - new loading animation for landing page - corrected form in Input module

--- src/app/modules/home/pages/home.component.ts
+++ src/app/modules/home/pages/home.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'camel-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
+
   }
+
+  ngAfterViewInit(): void {}
+
+
+
 
 }
