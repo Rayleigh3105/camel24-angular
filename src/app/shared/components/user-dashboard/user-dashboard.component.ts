5b6e6015a2c77ff1956f5d804524bc8fdf8db8ce
mvogt <moritz.vogt@vogges.de>
2019-02-12 19:43:19 +0100

    - create function for setting session storage - fetching user data when auftragsprofil is loaded - update ngModel variables with session storage

--- src/app/shared/components/user-dashboard/user-dashboard.component.ts
+++ src/app/shared/components/user-dashboard/user-dashboard.component.ts
@@ -22,7 +22,7 @@ export class UserDashboardComponent extends SessionStorageComponent implements O
 
     displayDialog: boolean;
     selectedCar: any;
-    order:any;
+    order: any;
     multiSortMeta: any;
     header: string;
 
@@ -46,6 +46,10 @@ export class UserDashboardComponent extends SessionStorageComponent implements O
 
     ngOnInit() {
         this.updateNgModelVariablesWithSessionStorage();
+        this.$authService.getCurrentUser().then(body => {
+            this.setSessionStorage(body);
+            this.updateNgModelVariablesWithSessionStorage();
+        });
         this.$dasboardService.getOrdersForUser().subscribe();
         this.multiSortMeta = [];
         this.multiSortMeta.push({field: 'createdAt', order: -1});
@@ -55,9 +59,9 @@ export class UserDashboardComponent extends SessionStorageComponent implements O
      * Updates the user based on the inoputs
      * @param form - form from the frontend
      */
-    async onUpdate(form : NgForm) {
-        if(form.valid) {
-            let userId : string = '';
+    async onUpdate(form: NgForm) {
+        if (form.valid) {
+            let userId: string = '';
             await this.$authService.getCurrentUser().then(response => {
                 userId = response._id;
                 console.log(userId);
@@ -66,7 +70,7 @@ export class UserDashboardComponent extends SessionStorageComponent implements O
             });
             // Create User object with updated values in it from the form
             let user: User = {
-                _id : userId,
+                _id: userId,
                 firstName: form.value.vorname,
                 lastName: form.value.nachname,
                 firmenName: form.value.firmenname,
@@ -76,38 +80,18 @@ export class UserDashboardComponent extends SessionStorageComponent implements O
                 land: form.value.land,
                 telefon: form.value.telefon,
                 plz: form.value.plz,
-                zusatz : form.value.zusatz,
+                zusatz: form.value.zusatz,
                 ansprechpartner: form.value.ansprechpartner
 
             };
 
-            this.$dasboardService.updateUser( user ).subscribe( body => {
+            this.$dasboardService.updateUser(user).subscribe(body => {
                 if (body !== null) {
-                    // @ts-ignore
-                    sessionStorage.setItem('firmenName', body.firmenName);
-                    // @ts-ignore
-                    sessionStorage.setItem('email', body.email);
-                    // @ts-ignore
-                    sessionStorage.setItem('adresse', body.adresse);
-                    // @ts-ignore
-                    sessionStorage.setItem('ort', body.ort);
-                    // @ts-ignore
-                    sessionStorage.setItem('land', body.land);
-                    // @ts-ignore
-                    sessionStorage.setItem('telefon', body.telefon);
-                    // @ts-ignore
-                    sessionStorage.setItem('plz', body.plz);
-                    // @ts-ignore
-                    sessionStorage.setItem('vorname', body.firstName);
-                    // @ts-ignore
-                    sessionStorage.setItem('nachname', body.lastName);
-                    // @ts-ignore
-                    sessionStorage.setItem('zusatz', body.zusatz);
-                    // @ts-ignore
-                    sessionStorage.setItem('ansprechpartner', body.ansprechpartner);
-
+                    // Set´s SessionStorage variables right
+                    this.setSessionStorage(body);
+                    // Updates SessionStorage with ngModel variables
                     this.updateNgModelVariablesWithSessionStorage();
-
+                    // Shows P-Message
                     this.showSuccess(body);
                 }
             }, error => {
@@ -185,5 +169,87 @@ export class UserDashboardComponent extends SessionStorageComponent implements O
         return order;
     }
 
+    /**
+     * Set`s SessionStorage for available data
+     * @param body
+     */
+    setSessionStorage(body) {
 
+        if (body.firmenName) {
+            // @ts-ignore
+            sessionStorage.setItem('firmenName', body.firmenName);
+        } else {
+            sessionStorage.removeItem('firmenName');
+        }
+
+        if (body.email) {
+            // @ts-ignore
+            sessionStorage.setItem('email', body.email);
+        } else {
+            sessionStorage.removeItem('email');
+        }
+
+        if (body.adresse) {
+            // @ts-ignore
+            sessionStorage.setItem('adresse', body.adresse);
+        } else {
+            sessionStorage.removeItem('adresse');
+        }
+
+        if (body.ort) {
+            // @ts-ignore
+            sessionStorage.setItem('ort', body.ort);
+        } else {
+            sessionStorage.removeItem('ort');
+        }
+
+        if (body.land) {
+            // @ts-ignore
+            sessionStorage.setItem('land', body.land);
+        } else {
+            sessionStorage.removeItem('land');
+        }
+
+        if (body.telefon) {
+            // @ts-ignore
+            sessionStorage.setItem('telefon', body.telefon);
+        } else {
+            sessionStorage.removeItem('telefon');
+        }
+
+        if (body.plz) {
+            // @ts-ignore
+            sessionStorage.setItem('plz', body.plz);
+        } else {
+            sessionStorage.removeItem('plz');
+        }
+
+        if (body.firstName) {
+            // @ts-ignore
+            sessionStorage.setItem('vorname', body.firstName);
+        } else {
+            sessionStorage.removeItem('vorname');
+        }
+
+        if (body.lastName) {
+            // @ts-ignore
+            sessionStorage.setItem('nachname', body.lastName);
+        } else {
+            sessionStorage.removeItem('nachname');
+        }
+
+        if (body.zusatz) {
+            // @ts-ignore
+            sessionStorage.setItem('zusatz', body.zusatz);
+        } else {
+            sessionStorage.removeItem('zusatz');
+        }
+
+        if (body.ansprechpartner) {
+            // @ts-ignore
+            sessionStorage.setItem('ansprechpartner', body.ansprechpartner);
+        } else {
+            sessionStorage.removeItem('ansprechpartner');
+        }
+    }
 }
