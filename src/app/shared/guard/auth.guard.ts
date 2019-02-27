9e1d7055a42e262b021093bd4985d05a205ddee0
mvogt <moritz.vogt@vogges.de>
2019-02-27 18:06:17 +0100

    - guard checks now if user has an valid token

--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -1,36 +1,43 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
-import { Observable } from 'rxjs';
+import {Observable} from 'rxjs';
+import {AuthService} from '../services/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  // Inject Router so we can hand off the user to the Login Page
-  constructor(private router: Router) {}
-
-  /**
-   * Checks if the session variable x-auth is available, if so the guard let´s pass the user
-   *
-   * @param next
-   * @param state
-   */
-  canActivate(
-      next: ActivatedRouteSnapshot,
-      state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-
-    if ( sessionStorage.getItem('x-auth') ){
-      // Token from the LogIn is avaiable, so the user can pass to the route
-      return true
-    } else  {
-      // Token from the LogIn is not avaible because something went wrong or the user wants to go over the url to the site
-      // Hands the user to the Landing page
-      alert("Sie sind momentan nicht eingeloggt. Bitte loggen Sie sich bevor besuch dieser Seite ein!");
-      this.router.navigate( [""] );
-      return false
-
+    // Inject Router so we can hand off the user to the Login Page
+    constructor(private router: Router, private $auth: AuthService) {
     }
 
-  }
+    /**
+     * Checks if the session variable x-auth is available, if so the guard let´s pass the user
+     *
+     * @param next
+     * @param state
+     */
+    async canActivate(
+        next: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Promise<boolean> {
+
+        let result: boolean = false;
+        await this.$auth.getCurrentUser()
+            .then(user => {
+                if (user) {
+                    result = true;
+                } else {
+                    alert('Sie sind momentan nicht eingeloggt. Bitte loggen Sie sich bevor besuch dieser Seite ein!');
+                    this.router.navigate(['']);
+                    result = false;
+                }
+            }).catch(() => {
+                alert('Sie sind momentan nicht eingeloggt. Bitte loggen Sie sich bevor besuch dieser Seite ein!');
+                this.router.navigate(['']);
+                result = false;
+            });
+        return result;
+
+    }
 }
