7e0a8e51bb46c25b40ceceb4a071cf330ede96c8
Moritz Vogt <moritz.vogt@vogges.de>
2019-12-02 22:19:33 +0100

    Correct Enpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    endpoint: "https://srv-1037770-1.camel-24.info/admindashboard/priceconfig",
+    endpoint: "https://srv-1037770-1.camel-24.info/",
     local: 'http://localhost:3000/'
 };
